d8bd01ba639a77a8c48423bf28b1fc3b2c47a305
Saanvi <saanviprashantkumar@gmail.com>
2024-10-28 14:58:15 +0530

    feat:add data models

---
+++ Eclipse/Models/Author.swift
@@ -0,0 +1,15 @@
+//
+//  Author.swift
+//  Eclipse
+//
+//  Created by user@87 on 28/10/24.
+//
+
+import Foundation
+
+struct Author {
+    var id: String
+    var name: String
+    var biography: String?
+    var books: [Book]
+}
